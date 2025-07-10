892109aaa7c22de303035ca8de6bfede585f6410
Mari Niniashvili <marininiashvili93@gmail.com>
2025-07-10 21:45:31 +0400

    basic converter

---
+++ converting.py
@@ -0,0 +1,15 @@
+
+def convert(ops, value):
+    if ops == "km":
+        return f"{ value * 1000}"
+
+    elif ops == "m":
+        return f"{ value / 1000}"
+
+    else: 
+        return "Error"
+
+ops = input(" m  or  km")
+value = int(input("enter number: "))
+
+print(convert(ops, value))
